b73666839fa99286f5861c91ba916e374a0b4238
Kasia Skonecka <kasia.skonecka@gmail.com>
2016-07-12 11:29:05 +0200

    js test

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener ("DOMContentLoaded", function (){
+
+var listingDropDownMenu = document.querySelector("#home_toolbar-select-share-type");
+listingDropDownMenu.style.display = "hidden";
+
+});
